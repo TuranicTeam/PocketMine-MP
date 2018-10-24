4777d2e97c373de0c9c7d97b13f61d8f3f7ec90d
Chaosfreak93 <yanluca.lentzen@gmail.com>
2018-10-24 16:23:50 +0200

    Readded Redstone

--- src/pocketmine/block/BlockFactory.php
+++ src/pocketmine/block/BlockFactory.php
@@ -204,7 +204,7 @@ class BlockFactory{
 		self::registerBlock(new MonsterSpawner());
 		self::registerBlock(new WoodenStairs(Block::OAK_STAIRS, 0, "Oak Stairs"));
 		self::registerBlock(new Chest());
-		//TODO: REDSTONE_WIRE
+		self::registerBlock(new Redstone());
 		self::registerBlock(new DiamondOre());
 		self::registerBlock(new Diamond());
 		self::registerBlock(new CraftingTable());
