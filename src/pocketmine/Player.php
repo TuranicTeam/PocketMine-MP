273e441b038a25b29f20d22af5b243f84cb2d1e1
TobiasDevDE <39413320+TobiasDevDE@users.noreply.github.com>
2018-11-17 09:46:45 +0100

    Fix Error Reported
    https://cdn.discordapp.com/attachments/501067947310317588/513255257044680714/IMG_20181117_103153.jpg

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -852,7 +852,11 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 	 * @return string
 	 */
 	public function getAddress() : string{
+		if($this->networkSession != null){
 		return $this->networkSession->getIp();
+		}else{
+			throw new \InvalidArgumentException("networkSession seems to be null!");
+		}
 	}
 
 	/**
