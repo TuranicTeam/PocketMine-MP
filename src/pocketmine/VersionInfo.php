cad33520379020358a493d423dbee9783453652b
NxtUserHD <50436997+NxtUser-HD@users.noreply.github.com>
2019-12-30 00:34:14 +0100

    Disabled Dev-Mode and edited version-name

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -29,7 +29,7 @@ if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
 const _VERSION_INFO_INCLUDED = true;
 
 
-const NAME = "Altay";
+const NAME = "Altay-PG";
 const BASE_VERSION = "3.11.3";
-const IS_DEVELOPMENT_BUILD = true;
+const IS_DEVELOPMENT_BUILD = false;
 const BUILD_NUMBER = 0;
